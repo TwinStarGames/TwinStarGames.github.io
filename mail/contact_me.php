3f3919bd82d8197682ac23a440d605b01b4dacf8
AhnHyoMin <Hulralra@gmail.com>
2018-04-21 01:24:27 +0900

    프로젝트 페이지 링크 추가

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -10,10 +10,10 @@ if(empty($_POST['name'])  		||
 	return false;
    }
 
-$name = strip_tags(htmlspecialchars('name'));
-$email_address = strip_tags(htmlspecialchars('email'));
-$phone = strip_tags(htmlspecialchars('phone'));
-$message = strip_tags(htmlspecialchars('message'));
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
 $to = 'greatkain76@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
